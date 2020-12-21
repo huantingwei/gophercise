ef1c30de550fc140f5794c25a81ea1ee9af2f2a1
huantingwei <ting02940@gmail.com>
2020-12-21 22:59:36 +0800

    feat: cmdline typing practice with time limit
    * flag: cmdline helper tool
    * csv: read csv file
    * strings: trim space
    * bufio: read input
    * go routine & go channel: self-defined time counter

---
+++ cmdQuiz/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type vocabulary struct {
+	v string
+}
+
+func main() {
+	// flag.String returns a string pointer
+	csvFileName := flag.String("csv", "vocabulary.csv", "a csv file in the format of word")
+	timeLimit := flag.Int("t", 20, "time limit in seconds")
+	flag.Parse()
+
+	// read file
+	// dereference
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open file: %s\n", *csvFileName))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("Failed to parse file\n"))
+	}
+
+	// parse file
+	vocabs := parseLine(lines)
+
+	// get ready
+	fmt.Printf("Ready?\n")
+	for _, i := range []int{3, 2, 1} {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+
+	// game start
+	// start time count down
+	ch := make(chan bool, 1)
+	correct := make(chan int, 1)
+
+	fmt.Printf("Go!!!\n\n")
+	go timeCounter(*timeLimit, ch)
+	go question(vocabs, correct)
+
+	// loop until game over or finish
+	for {
+		select {
+		case _, ok := <-ch:
+			if ok {
+				fmt.Printf("\n\nGame Over!")
+			}
+			return
+		case e, ok := <-correct:
+			if ok {
+				fmt.Printf("Congratulations!!!\n You finished!!!\n\n")
+				fmt.Printf("You've got %d out of %d.\n", e, len(vocabs))
+			}
+			return
+		}
+	}
+
+}
+
+func timeCounter(limit int, ch chan<- bool) {
+	for i := 0; i < limit; i++ {
+		time.Sleep(1 * time.Second)
+	}
+	ch <- true
+}
+
+func question(vocabs []vocabulary, correct chan<- int) {
+	ret := 0
+	for _, p := range vocabs {
+		fmt.Printf("%s : ", p.v)
+		input := bufio.NewReader(os.Stdin)
+		text, _ := input.ReadString('\n')
+
+		if strings.TrimSpace(text) == p.v {
+			ret += 1
+		}
+	}
+	correct <- ret
+}
+
+func parseLine(lines [][]string) []vocabulary {
+
+	// if already know the length
+	// declare it
+	problems := make([]vocabulary, len(lines))
+
+	for i, l := range lines {
+		problems[i] = vocabulary{
+			v: strings.TrimSpace(l[0]),
+		}
+	}
+	return problems
+}
+
+func exit(msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
+}
