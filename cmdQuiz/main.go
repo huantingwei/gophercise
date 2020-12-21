1ff15fdf596d3cae57a27acfd578f9ed840688d3
huantingwei <ting02940@gmail.com>
2020-12-21 23:57:21 +0800

    add demo functions from gophercise course
    * time.NewTimer
    * different select/case logic

--- cmdQuiz/main.go
+++ cmdQuiz/main.go
@@ -15,6 +15,11 @@ type vocabulary struct {
 }
 
 func main() {
+	// demoFromCourse()
+	demoFromMyself()
+}
+
+func demoFromMyself() {
 	// flag.String returns a string pointer
 	csvFileName := flag.String("csv", "vocabulary.csv", "a csv file in the format of word")
 	timeLimit := flag.Int("t", 20, "time limit in seconds")
@@ -45,10 +50,13 @@ func main() {
 
 	// game start
 	// start time count down
+
+	// self-defined timer
 	ch := make(chan bool, 1)
 	correct := make(chan int, 1)
 
 	fmt.Printf("Go!!!\n\n")
+
 	go timeCounter(*timeLimit, ch)
 	go question(vocabs, correct)
 
@@ -109,4 +117,64 @@ func parseLine(lines [][]string) []vocabulary {
 func exit(msg string) {
 	fmt.Printf(msg)
 	os.Exit(1)
+}
+
+func demoFromCourse() {
+	// flag.String returns a string pointer
+	csvFileName := flag.String("csv", "vocabulary.csv", "a csv file in the format of word")
+	timeLimit := flag.Int("t", 20, "time limit in seconds")
+	flag.Parse()
+
+	// read file
+	// dereference
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open file: %s\n", *csvFileName))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("Failed to parse file\n"))
+	}
+
+	// parse file
+	vocabs := parseLine(lines)
+
+	// get ready
+	fmt.Printf("Ready?\n")
+	for _, i := range []int{3, 2, 1} {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Printf("Go!!!\n\n")
+
+	// game start
+	// start time count down
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+	for _, p := range vocabs {
+		fmt.Printf("%s : ", p.v)
+		ansCh := make(chan string)
+		go func() {
+			var ans string
+			fmt.Scanf("%s\n", &ans)
+			ansCh <- ans
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(vocabs))
+			return
+		case ans, ok := <-ansCh:
+			if ok {
+				if ans == p.v {
+					correct++
+				}
+			}
+		}
+	}
+	fmt.Printf("\nCongratulations!!!\nYou finished!!!\n")
+	fmt.Printf("You've got %d out of %d.\n", correct, len(vocabs))
 }
