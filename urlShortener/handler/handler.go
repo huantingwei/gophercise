91893b34910124e6f7ddf0366f49bf53d2ae68a2
huantingwei <ting02940@gmail.com>
2020-12-22 23:27:17 +0800

    feat: urlShortener v1
    * net.http: Handler, HandlerFunc, Redirect
    * gopkg.in/yaml.v2

---
+++ urlShortener/handler/handler.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+// http.HandlerFunc = a function
+// http.Handler has a method ServeHTTP(ResponseWriter, *Request)
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if path, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, path, http.StatusSeeOther)
+			return
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+	// parse yaml file
+	type pathUrl struct {
+		Path string `yaml:"path"`
+		URL  string `yaml:"url"`
+	}
+
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	// convert to map[string]string
+	pathUrlMap := map[string]string{}
+	for _, v := range pathUrls {
+		pathUrlMap[v.Path] = v.URL
+	}
+
+	return MapHandler(pathUrlMap, fallback), nil
+}
