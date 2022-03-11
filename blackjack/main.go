9a14601e92b4e05a2059c7fe356458721e8afbfb
huantingwei <ting02940@gmail.com>
2022-03-11 13:31:10 +0800

    calculate player's total in add()

--- blackjack/main.go
+++ blackjack/main.go
@@ -8,36 +8,16 @@ import (
 )
 
 // https://bicyclecards.com/how-to-play/blackjack/
-// Deck
-// shuffle
-// deal - player
-// deal - dealer
-// Player
-// .cards
-// stand()
-// hit()
-// isNatural
-// isBusted
-
-// six-deck game (312 cards)
-// A = 11 or 1
-// JQK = 10
-
-// every player place bets
 // player: 1 face up to everyone, 1 face up to themselves
 // dealer: 1 face up to everyone, 1 face down
-
-// first 2 cards = "natural" -> blackjack
-// => dealer pays 1.5x of the player's bet
-// => other players pay 1x of the player's bet
-
 //  "stand" or "hit"
 
 // When the dealer has served every player, the dealers face-down card is turned up.
 // If the total is 17 or more, it must stand.
 // If the total is 16 or under, they must take a card.
 // The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand.
-// If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand
+// If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21),
+//  the dealer must count the ace as 11 and stand
 
 const MAX = 21
 const NUM_DECK = 3
@@ -88,37 +68,8 @@ func getPoint(card string) int {
 }
 
 func (p *Player) check(threshold int) bool {
-	// go through every card again
-	// better version: just check the newest card
-	var possibilities []int
-	for _, card := range p.cards {
-		if card == "A" {
-			// for each possibilities, duplicate
-			if len(possibilities) == 0 {
-				possibilities = append(possibilities, 1)
-				possibilities = append(possibilities, 11)
-			} else {
-				for _, pos := range possibilities {
-					pos += 1
-					possibilities = append(possibilities, pos+11)
-				}
-			}
-		} else {
-			if len(possibilities) == 0 {
-				possibilities = append(possibilities, getPoint(card))
-			} else {
-				for i := 0; i < len(possibilities); i++ {
-					possibilities[i] += getPoint(card)
-				}
-			}
-		}
-	}
-
-	// set possible totals
-	p.total = possibilities
-	// check if over the threshold
 	m := 0
-	for i, pos := range possibilities {
+	for i, pos := range p.total {
 		if i == 0 || pos < m {
 			m = pos
 		}
@@ -128,6 +79,37 @@ func (p *Player) check(threshold int) bool {
 
 func (p *Player) add(card string) {
 	p.cards = append(p.cards, card)
+
+	if card == "A" {
+		if len(p.total) == 0 {
+			p.total = append(p.total, 1)
+			p.total = append(p.total, 11)
+		} else {
+			for i := 0; i < len(p.total); i++ {
+				p.total[i] += 1
+				p.total = append(p.total, p.total[i]+11)
+			}
+		}
+	} else {
+		if len(p.total) == 0 {
+			p.total = append(p.total, getPoint(card))
+		} else {
+			for i := 0; i < len(p.total); i++ {
+				p.total[i] += getPoint(card)
+			}
+		}
+	}
+}
+
+func (p *Player) Init(name string) {
+	p.total = make([]int, 0)
+	p.cards = make([]string, 0)
+	p.role = name
+	p.isBusted = false
+}
+
+func (p *Player) SetName(name string) {
+	p.role = name
 }
 
 func (p *Player) Hit(deck *Deck) {
@@ -135,34 +117,58 @@ func (p *Player) Hit(deck *Deck) {
 }
 
 func (p *Player) CheckIsOver(threshold int) bool {
-	return p.check(threshold)
+	isOver := p.check(threshold)
+	if threshold == MAX && isOver {
+		p.isBusted = true
+	}
+	return isOver
 }
 
 func (p *Player) Show() {
-	fmt.Printf("%s has:\n", p.role)
+	fmt.Printf("\n%s has:\n", p.role)
 	for i, card := range p.cards {
 		fmt.Printf("| %2d: %2v ", i, card)
 		fmt.Printf("|\n")
 	}
-	fmt.Println()
+	fmt.Printf("Total = ")
+	for i, t := range p.total {
+		if i != 0 {
+			fmt.Printf("or %2d ", t)
+		} else {
+			fmt.Printf("%2d ", t)
+		}
+	}
+	fmt.Printf("\n\n==============================\n")
+	fmt.Printf("%10s's best score = %2d\n", p.role, p.BestScore())
+	fmt.Printf("==============================\n")
 }
 
-func (p *Player) SetName(name string) {
-	p.role = name
+func (p *Player) BestScore() int {
+	if len(p.total) == 0 {
+		return 0
+	}
+
+	best := p.total[0]
+	for i := 1; i < len(p.total); i++ {
+		if p.total[i] <= MAX && p.total[i] > best {
+			best = p.total[i]
+		}
+	}
+	return best
 }
 
 func main() {
-	cards := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	cards := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	rand.Seed(time.Now().UnixNano())
 	var deck Deck
-	for i := 0; i < NUM_DECK; i++ {
+	for i := 0; i < 4*NUM_DECK; i++ {
 		deck.cards = append(deck.cards, cards...)
 	}
 
 	var player, dealer Player
 	var action string
-	player.SetName("Player 1")
-	dealer.SetName("Dealer")
+	player.Init("Player 1")
+	dealer.Init("Dealer")
 
 	fmt.Printf("Shuffling...\n")
 	deck.Shuffle()
@@ -181,33 +187,33 @@ func main() {
 	for {
 		if !dealer.CheckIsOver(16) {
 			dealer.Hit(&deck)
-			dealer.Show()
 		} else {
 			break
 		}
 	}
+	dealer.Show()
 
 	fmt.Printf("Player's turn!!!\n")
 	for {
-		fmt.Printf("Hit or Stand? (hit or stand))\n")
-		fmt.Scanf("%s", &action)
-		fmt.Printf("%s", action)
-		if action == "hit" {
-			player.Hit(&deck)
-			player.Show()
-
-		} else if action == "stand" {
-			break
-		} else {
-			fmt.Printf("Please enter hit or stand\n")
-		}
 		if player.CheckIsOver(MAX) {
 			fmt.Printf("You are busted!\n")
 			break
 		}
+
+		player.Show()
+		fmt.Printf("Hit or Stand? (hit or stand)\n")
+		fmt.Scanf("%s", &action)
+
+		if action == "hit" || action == "h" || action == "H" {
+			player.Hit(&deck)
+		} else if action == "stand" || action == "s" || action == "S" {
+			break
+		} else {
+			fmt.Printf("Please enter `hit` or `stand`\n")
+		}
+
 	}
 	fmt.Printf("Show hands!\n")
 	player.Show()
 	dealer.Show()
-
 }
